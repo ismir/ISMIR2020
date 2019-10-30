fefab6a72a9835328000e95591bf15488a0bcda6
evansavage <esav678@gmail.com>
2019-10-30 14:02:08 -0400

    Removed unneeded js code in scripts.js

--- _js/scripts.js
+++ _js/scripts.js
@@ -156,95 +156,95 @@ function ShowHideNav() {
 /* HANDLE MODAL */
 /* -----------------------------------------------------------------------*/
 
-function openModal() {
-    $( "body" ).css( "overflow", "hidden" );
-    $( ".modal, .modal__overlay" ).show().css( "display", "flex" );
-    $( ".modal__inner" ).velocity( { translateY: 0, opacity: 1 } );
-    $( ".modal__overlay" ).velocity( { opacity: 1 }, 100 );
-}
-
-function removeModal() {
-    $( "body" ).css( { "overflow": "visible" } );
-    $( ".modal, .modal__overlay, .modal__inner" ).velocity( { opacity: 0 }, function() {
-        $( ".modal" ).css( { opacity: 1 } );
-        $( ".modal__inner" ).css( {
-            "-webkit-transform": "translateY(200px)",
-            "-ms-transform": "translateY(200px)",
-            transform: "translateY(200px)"
-        } );
-        $( ".modal, .modal__overlay" ).hide();
-        $( ".modal__body" ).empty();
-    } );
-}
-
-$( ".js-modal-close" ).click( function() {
-    removeModal();
-} );
-
-$( ".modal__overlay" ).click( function() {
-    removeModal();
-} );
+// function openModal() {
+//     $( "body" ).css( "overflow", "hidden" );
+//     $( ".modal, .modal__overlay" ).show().css( "display", "flex" );
+//     $( ".modal__inner" ).velocity( { translateY: 0, opacity: 1 } );
+//     $( ".modal__overlay" ).velocity( { opacity: 1 }, 100 );
+// }
+//
+// function removeModal() {
+//     $( "body" ).css( { "overflow": "visible" } );
+//     $( ".modal, .modal__overlay, .modal__inner" ).velocity( { opacity: 0 }, function() {
+//         $( ".modal" ).css( { opacity: 1 } );
+//         $( ".modal__inner" ).css( {
+//             "-webkit-transform": "translateY(200px)",
+//             "-ms-transform": "translateY(200px)",
+//             transform: "translateY(200px)"
+//         } );
+//         $( ".modal, .modal__overlay" ).hide();
+//         $( ".modal__body" ).empty();
+//     } );
+// }
+//
+// $( ".js-modal-close" ).click( function() {
+//     removeModal();
+// } );
+//
+// $( ".modal__overlay" ).click( function() {
+//     removeModal();
+// } );
 
 /*-------------------------------------------------------------------------*/
 /* FORM VALIDATION */
 /* -----------------------------------------------------------------------*/
 
-function formCheck() {
-    $( ".js-submit" ).click( function( e ) {
-
-        e.preventDefault();
-
-        var $inputs = $( ".form__input input" );
-        var textarea = $( ".form__input textarea" );
-        var isError = false;
-
-        $( ".form__input" ).removeClass( "error" );
-        $( ".error-data" ).remove();
-
-        for ( var i = 0; i < $inputs.length; i++ ) {
-            var input = $inputs[ i ];
-            if ( $( input ).attr( "required", true ) && !validateRequired( $( input ).val() ) ) {
-
-                addErrorData( $( input ), "This field is required" );
-
-                isError = true;
-            }
-            if ( $( input ).attr( "required", true ) && $( input ).attr( "type" ) === "email" && !validateEmail( $( input ).val() ) ) {
-                addErrorData( $( input ), "Email address is invalid" );
-                isError = true;
-            }
-            if ( $( textarea ).attr( "required", true ) && !validateRequired( $( textarea ).val() ) ) {
-                addErrorData( $( textarea ), "This field is required" );
-                isError = true;
-            }
-        }
-        if ( isError === false ) {
-            $( "#contactForm" ).submit();
-        }
-    } );
-}
-
-// Validate if the input is not empty
-function validateRequired( value ) {
-    if ( value === "" ) {
-      return false;
-    }
-    return true;
-}
-
-// Validate if the email is using correct format
-function validateEmail( value ) {
-    if ( value !== "" ) {
-        return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i.test( value );
-    }
-    return true;
-}
-
-// Add error message to the input
-function addErrorData( element, error ) {
-    element.parent().addClass( "error" );
-    element.after( "<span class='error-data'>" + error + "</span>" );
-}
+// function formCheck() {
+//     $( ".js-submit" ).click( function( e ) {
+//
+//         e.preventDefault();
+//
+//         var $inputs = $( ".form__input input" );
+//         var textarea = $( ".form__input textarea" );
+//         var isError = false;
+//
+//         $( ".form__input" ).removeClass( "error" );
+//         $( ".error-data" ).remove();
+//
+//         for ( var i = 0; i < $inputs.length; i++ ) {
+//             var input = $inputs[ i ];
+//             if ( $( input ).attr( "required", true ) && !validateRequired( $( input ).val() ) ) {
+//
+//                 addErrorData( $( input ), "This field is required" );
+//
+//                 isError = true;
+//             }
+//             if ( $( input ).attr( "required", true ) && $( input ).attr( "type" ) === "email" && !validateEmail( $( input ).val() ) ) {
+//                 addErrorData( $( input ), "Email address is invalid" );
+//                 isError = true;
+//             }
+//             if ( $( textarea ).attr( "required", true ) && !validateRequired( $( textarea ).val() ) ) {
+//                 addErrorData( $( textarea ), "This field is required" );
+//                 isError = true;
+//             }
+//         }
+//         if ( isError === false ) {
+//             $( "#contactForm" ).submit();
+//         }
+//     } );
+// }
+//
+// // Validate if the input is not empty
+// function validateRequired( value ) {
+//     if ( value === "" ) {
+//       return false;
+//     }
+//     return true;
+// }
+//
+// // Validate if the email is using correct format
+// function validateEmail( value ) {
+//     if ( value !== "" ) {
+//         return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i.test( value );
+//     }
+//     return true;
+// }
+//
+// // Add error message to the input
+// function addErrorData( element, error ) {
+//     element.parent().addClass( "error" );
+//     element.after( "<span class='error-data'>" + error + "</span>" );
+// }
 
 
 /*-------------------------------------------------------------------------*/
