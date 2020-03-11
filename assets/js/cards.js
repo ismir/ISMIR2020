41ea4970ac52f3b8acf397df57ceeaa2b2db3276
evansavage <esav678@gmail.com>
2020-03-11 13:22:17 -0400

    Fixed city card hover formatting

---
+++ assets/js/cards.js
@@ -0,0 +1,10 @@
+// $(".city-card").hover(function() {
+//   $('.city-card .card-background').css({
+//     // '-webkit-transform': 'scale(1.08)',
+//     'transform': 'scale(1.08)',
+//     // 'height': '50%',
+//   });
+//   console.log('yes');
+// });
+//
+// console.log('yes');
